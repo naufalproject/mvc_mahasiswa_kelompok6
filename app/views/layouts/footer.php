bcd9243fe9fa3c29dc6400a9c3e26fb7c9a29434
RETNO FAJAR JAYANTI <retnofajarjayanti@gmail.com>
2026-05-15 01:04:06 +0800

    Sesi 7 Layout Dengan Bootstrap & Responsivitas

---
+++ app/views/layouts/footer.php
@@ -0,0 +1,8 @@
+<footer class="text-center mt-5 pb-3">
+        <hr>
+        <small class="text-muted">&copy; 2026 - kelompok 4 praktikum 6C TI</small>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
